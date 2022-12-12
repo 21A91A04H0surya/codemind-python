1b30b9552d2d46a549500274f3bbbcb5c21cf425
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2022-12-12 19:28:33 +0530

    n=input() l=list(n) c=0 f=0 v="aeiou" k=[] p=0 for i in l:     if i==" ":         k.append(c)         c=0         continue     if i in v:         c+=1 k.append(c) z=min(k) for j in k:     if j==z:         p+=1 print(p)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,24 @@
+n=input()
+l=list(n)
+c=0
+f=0
+v="aeiou"
+k=[]
+p=0
+for i in l:
+    if i==" ":
+        k.append(c)
+        c=0
+        continue
+    if i in v:
+        c+=1
+k.append(c)
+z=min(k)
+for j in k:
+    if j==z:
+        p+=1
+print(p)
+
+
+        
+    
